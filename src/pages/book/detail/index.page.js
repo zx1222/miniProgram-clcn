a8ca8f02ebfcf92127eb3d11c7b867b937e1a930
zhangxin <1655638981@qq.com>
2019-12-05 11:29:16 +0800

    实现图书详情模板

--- src/pages/book/detail/index.page.js
+++ src/pages/book/detail/index.page.js
@@ -3,20 +3,45 @@ import ApiService from '@/utils/api.js';
 export default {
   data() {
     return {
-      data: [],
-      limits: 10,
-      currentPage: 1,
-      totalCount: 1,
-      isLoadedAll: false,
-      isLoadingMore: false,
+      currentIndex: 0,
+      detail: {
+        images: 'http://primo.clcn.net.cn:8991/cgi-bin/isbn_cover.cgi?isbn=978-7-111-61272-8',
+        title: 'java xxxx',
+        publisher: "出版社: 北京;机械工业出版社;2019",
+        author: '(日)河合俊雄著 ; 冯莹莹译',
+        category_name: '中文',
+        simple_intro: '本书是一本由村上式写作谈故事中的心理学。日本作家村上春树的小说《1Q84》上市12天狂销百万册，刷新日本纪录。他本人更是屡次问鼎诺贝尔文学奖，书迷遍及全球。为什么村上的小说如此迷人？他到底写中了现代人心中的什么？身为村上书迷的荣格心理学者河合俊雄，尝试以心理学来探究这个问题。河合俊雄说：“荣格心理疗法的世界重视梦与意象，许多时候与现实或常识是不相同的。这和村上春树所描绘的事物非常呼应。”而做为村上春树的读者，往往能经由流畅易读的小说进入与现实不同的次元，触碰到人们共同的基底而转化自己，这或许就是其作品魅力所在'
+      },
+      locations: [
+        {
+          id:1,
+          location: '朝阳区图书馆',
+          status: '在架上',
+          location_detail: '朝阳区图书馆自主图书馆02',
+          category_name: '中文图书',
+          identical_number: '1552.45/23',
+          codebar: '0524565585655'
+        },
+        {
+          id:2,
+          location: '朝阳区图书馆',
+          status: '在架上',
+          location_detail: '朝阳区图书馆自主图书馆02',
+          category_name: '中文图书',
+          identical_number: '1552.45/23',
+          codebar: '0524565585655'
+        }
+      ],
+      activeCollapse: []
+
     }
   },
   created() {
-    this.api=new ApiService();
+    this.api = new ApiService();
 
   },
   mounted() {
-    this.getData(1)
+    this.getData()
   },
 
   components: {
@@ -26,46 +51,21 @@ export default {
 
   },
   methods: {
-    getData(index) {
-      index == 1 ? wx.showLoading({
-        title: '加载中',
-      }) : '';
-      this.api.get('bplisn/books')({
-        'per-page': this.limits,
-        page: index,
-      }).subscribe(res => {
-        this.isLoadingMore = false;
-        wx.hideLoading();
-        let resData = res.items;
-        let data = this.data;
-        data = data.concat(resData)
-        this.data = data;
-        this.totalCount = res._meta.totalCount;
-        if (this.currentPage * this.limits >= this.totalCount) {
-          this.isLoadedAll = true
-        }
-        console.log(this.data)
-      })
+    tabChange(e) {
+      this.data = [];
+      this.currentPage = 1;
+      this.isLoadedAll = false;
+      this.currentIndex = e.mp.detail.index;
+      // this.currentIndex == 0 ? this.getEventsData(1) : this.getBooksData(1)
     },
 
-    loadMore() {
-      if (!this.isLoadedAll) {
-        this.currentPage = this.currentPage + 1;
-        this.isLoadingMore = true
-        this.getData(this.currentPage)
-      }
+    collapseChange(e) {
+      console.log(e)
+      this.activeCollapse = e.mp.detail
     },
 
-    navigateDetail(id) {
-      console.log(id)
-      wx.navigateTo({
-        url: `/pages/book/detail/main?id=${id}`
-      })
+    getData() {
+
     }
-  },
-
-  onReachBottom() {
-    console.log('到底啦')
-    this.loadMore();
   },
 }
--- src/pages/book/detail/index.vue
+++ src/pages/book/detail/index.vue
@@ -1,38 +1,81 @@
 <template>
-  <div class="book-page-wrap">
-    <div class="book-list-wrap">
-      <div
-        class="item flex"
-        v-for="(item,index) in data"
-        :key="index"
-        @click="navigateDetail(item.id)"
-      >
-        <img :src="item.image" class="book-cover" mode="widthFix" />
-        <div class="book-info flex-c">
-          <h4 class="title">{{item.title}}</h4>
-          <p class="text-s text-darkgray">{{item.author}}</p>
-          <p class="text-s text-darkgray">{{item.publisher}}</p>
-          <p class="text-s text-darkgray text-overflow2">{{item.simple_intro}}</p>
-        </div>
-      </div>
-    </div>
-    <div
-      class="w100 loading-tips flex-c align-items-center justify-content-center text-gray text-s"
-      v-if="isLoadingMore"
-    >
-      <img src="/static/images/paging-loading.gif" class="paging-loading" />
-      加载中
+  <div class="book-detail-page-wrap">
+    <div class="book-cover flex align-items-center justify-content-center">
+      <img mode="widthFix" class="represent-img" :src="detail.images" />
     </div>
 
-    <div class="w100 loading-tips text-center loading-wrap" v-if="isLoadedAll&&data.length==0">暂无数据</div>
-    <div
-      class="w100 loading-tips text-center loading-wrap"
-      v-if="isLoadedAll&&data.length!=0"
-    >已无更多数据</div>
+    <van-tabs color="#007dff" active="currentIndex" @change="tabChange">
+      <van-tab title="详细信息">
+        <div class="bookinfo-wrap">
+          <p class="text-blue blod title">{{detail.title}}</p>
+          <div class="row flex justify-content-between">
+            <div class="row-label">作者：</div>
+            <div class="row-content">{{detail.author}}</div>
+          </div>
+          <div class="row flex justify-content-between">
+            <div class="row-label">出版社：</div>
+            <div class="row-content">{{detail.publisher}}</div>
+          </div>
+          <div class="row flex justify-content-between">
+            <div class="row-label">语种：</div>
+            <div class="row-content">{{detail.category_name}}</div>
+          </div>
+          <div class="row flex justify-content-between">
+            <div class="row-label">标识符：</div>
+            <div class="row-content">ISBN7-5057-2246-8</div>
+          </div>
+          <div class="row flex justify-content-between">
+            <div class="row-label">主题：</div>
+            <div class="row-content">明代</div>
+          </div>
+          <div class="row flex justify-content-between">
+            <div class="row-label">载体形式：</div>
+            <div class="row-content">143 页；彩图；21cm</div>
+          </div>
+          <div class="row flex justify-content-between">
+            <div class="row-label">摘要：</div>
+            <div class="row-content">{{detail.simple_intro}}</div>
+          </div>
+        </div>
+      </van-tab>
+
+      <van-tab title="馆藏地">
+        <div class="locations-wrap">
+          <van-collapse :value="activeCollapse" @change="collapseChange">
+            <van-collapse-item
+              v-for="(item,index) in locations"
+              :title="item.location"
+              :key="index"
+              :name="item.id"
+            >
+              <div class="row flex justify-content-between">
+                <div class="row-label">馆藏地：</div>
+                <div class="row-content">{{item.location_detail}}</div>
+              </div>
+              <div class="row flex justify-content-between">
+                <div class="row-label">索书号：</div>
+                <div class="row-content">{{item.identical_number}}</div>
+              </div>
+              <div class="row flex justify-content-between">
+                <div class="row-label">状态：</div>
+                <div class="row-content">{{item.status}}</div>
+              </div>
+              <div class="row flex justify-content-between">
+                <div class="row-label">条码：</div>
+                <div class="row-content">{{item.codebar}}</div>
+              </div>
+              <div class="row flex justify-content-between">
+                <div class="row-label">资料类型：</div>
+                <div class="row-content">{{item.category_name}}</div>
+              </div>
+            </van-collapse-item>
+          </van-collapse>
+        </div>
+      </van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script src='./index.page.js'></script>
 
 <style scoped src='./index.page.css'></style>
-  
