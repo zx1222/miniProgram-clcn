f0da0f76522e7913512a94342a69ba6bdbf7c2b9
zhangxin <1655638981@qq.com>
2019-12-09 18:45:16 +0800

    模拟坐标换成北京的坐标

--- src/pages/location/map/index.page.js
+++ src/pages/location/map/index.page.js
@@ -7,14 +7,14 @@ export default {
     return {
       mapHeight: wx.getSystemInfoSync().windowHeight,
       mapWidth: wx.getSystemInfoSync().windowWidth,
-      latitude: 23.099994,
-      longitude: 113.324520,
+      latitude: 39.92855,
+      longitude: 116.41637,
       markers: [
         {
           iconPath: "/static/images/location-fill.png",
           id: 0,
-          latitude: 23.099994,
-          longitude: 113.324520,
+          latitude: 39.92855,
+          longitude: 116.41637,
           width: 30,
           height: 30,
           title: 'place1'
@@ -22,8 +22,17 @@ export default {
         {
           iconPath: "/static/images/location-fill.png",
           id: 1,
-          latitude: 23.199994,
-          longitude: 113.304520,
+          latitude: 39.92455,
+          longitude: 116.4163,
+          width: 30,
+          height: 30,
+          title: 'place2'
+        },
+        {
+          iconPath: "/static/images/location-fill.png",
+          id: 1,
+          latitude: 39.92355,
+          longitude: 116.42627,
           width: 30,
           height: 30,
           title: 'place2'
@@ -39,6 +48,7 @@ export default {
   mounted() {
     // this.mapCtx = wx.createMapContext('map');
     // this.openLocation()
+    // this.getLocation()
   },
 
   components: {
@@ -60,6 +70,41 @@ export default {
       wx.openLocation({
         latitude: lat,
         longitude: long
+      })
+    },
+
+    getLocation() {
+      wx.getLocation({
+        type: 'wgs84',
+        success: function (res) {
+          console.log(res)
+          // 如果打开地图没有选择地址则跳转到原H5页面，否则选择地址后再进行跳转
+          this.latitude = res.latitude;
+          this.longitude = res.longitude
+        },
+
+        // fail: function (res) {
+        //   wx.getSetting({
+        //     success: (res) => {
+        //       if (res.authSetting['scope.userLocation'] != true) { //非初始化进入该页面,且未授权,按钮显示
+        //         that.setData({
+        //           isLocationAuthorize: false,
+        //         })
+        //       } else {
+        //         wx.showModal({ // 手机定位未开启状态
+        //           title: '手机定位未开启',
+        //           content: '请在手机设置中打开定位，我们需要知道您的位置才能提供更好的服务~',
+        //           showCancel: false,
+        //           success: function (res) {
+        //             wx.navigateBack({
+        //               delta: 1
+        //             })
+        //           }
+        //         })
+        //       }
+        //     }
+        //   })
+        // }
       })
     }
 
